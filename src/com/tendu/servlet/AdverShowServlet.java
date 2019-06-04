11f2e1517ada3dcedb008648832532f5951493ff
modestyli <2504757738@qq.com>
2019-06-04 22:59:05 +0800

    adver首页功能完善

--- src/com/tendu/servlet/AdverShowServlet.java
+++ src/com/tendu/servlet/AdverShowServlet.java
@@ -13,20 +13,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/AdverInfoServlet")
-public class AdverInfoServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request,response);
-    }
-
+@WebServlet ("/AdverShowServlet")
+public class AdverShowServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Integer id = Integer.parseInt(request.getParameter("id"));
         //链接数据库并传值
         SqlSession session = DBTools.getSession();
         AdverMapper adverMapper = session.getMapper(AdverMapper.class);
         List<Adver> list = adverMapper.selectAdverById(id);
+        List<Adver> All = adverMapper.queryAllAdver();
         request.setAttribute("adver",list.get(0));
+        request.setAttribute("alladvers",All);
         session.close();
-        request.getRequestDispatcher("Adver/show.jsp").forward(request, response);
+        request.getRequestDispatcher("index/advershow.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
     }
 }
